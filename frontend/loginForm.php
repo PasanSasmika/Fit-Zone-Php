574322baafaede8895dd13828f0d92f0a0d07e69
PasanSasmika <pasansasmika198@gmail.com>
2025-03-23 10:22:35 +0530

    content update

--- frontend/loginForm.php
+++ frontend/loginForm.php
@@ -14,7 +14,7 @@ if(isset($_POST["submit"])){
             
             $_SESSION["login"] = true;
             $_SESSION["id"] = $row["id"];
-            header("location: indexLog.php"); 
+            header("location: index.php"); 
             exit();
         }
         else{
